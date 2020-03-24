8b6d0f9bbd61882b292003b90ebf0cd6a0aff17d
DablSi <ka_nahele@yandex.ru>
2020-03-24 21:41:16 +0300

    Initial commit

---
+++ Assets/FollowPlayer.cs
@@ -0,0 +1,13 @@
+﻿using UnityEngine;
+
+public class FollowPlayer : MonoBehaviour
+{
+    // Player's Transform component
+    public Transform player;
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
---
+++ Assets/PlayerMovement.cs
@@ -0,0 +1,40 @@
+﻿using UnityEngine;
+
+public class PlayerMovement : MonoBehaviour {
+
+    public Rigidbody rb;
+    public float forwardForce = 1000f;
+    public float sidewaysForce = 500f;
+    private int direction = 0;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    // If working with the physics use FixedUpdate()
+    void FixedUpdate()
+    {
+        // deltaTime is used for force to be the same throughout different FPS
+        rb.AddForce(sidewaysForce * direction * Time.deltaTime, 0, forwardForce * Time.deltaTime);
+    }
+
+    // If we are working with user input it's better to use Update() to avoid dela
+    private void Update()
+    {
+        if (Input.GetKey(KeyCode.D))
+        {
+            direction = 1;
+        }
+        else if (Input.GetKey(KeyCode.A))
+        {
+            direction = -1;
+        }
+        else
+        {
+            direction = 0;
+        }
+    }
+}
